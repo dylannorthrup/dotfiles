65d94fb31430c9dd3fd4c5f470e1a1f9f4321aaf
Dylan Northrup <dylan.northrup@turner.com>
2013-04-29 12:07:34 -0400

    Making push_dotfiles much more modular and, therefore, awesome

--- push_dotfiles.sh
+++ push_dotfiles.sh
@@ -1,15 +1,68 @@
 #!/bin/bash
 
-for i in $(grep -v '^#' filelist); do
-  echo Differences for $i:;
-  diff $i ~/$i | grep -v ^Only
-  echo "Press ENTER to continue"
-  read do_it
-  # Need to do detection for directories since OS X doesn't do a proper recursive copy 
-  # without the trailing slash(es)
-  if [ -d $i ]; then
-    cp -rp $i/ ~/$i/
+diff_file() {
+  fname=$1
+  echo Differences for $fname:;
+  diff $fname ~/$fname
+  if [ $? -lt 1 ]; then
+    return 0
+  fi
+  return 1
+}
+
+diff_dir() {
+  dname=$1
+  echo Differences for $dname:;
+  diff $dname ~/$dname | egrep '[<>]'
+  if [ $? -gt 0 ]; then
+    return 1
+  fi
+  return 0
+}
+
+confirm_copy() {
+  fname=$1
+  read -p "Copy ${fname} (Y/N) " -n 1
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    return 1
+  fi
+  echo "Skipping copy for ${fname}"
+  return 0
+}
+
+copy_file() {
+  echo cp -rp $1 ~/$1
+}
+
+check_and_copy_file() {
+  fname=$1
+  diff_file $fname
+  if [ $? -gt 0 ]; then
+    confirm_copy $fname
+    if [ $? -gt 0 ]; then
+      # Build out dir structure if necessary
+      DIRNAME=$(dirname $fname)
+      if [ ! -d $DIRNAME ]; then
+        mkdir $DIRNAME
+      fi
+      copy_file $fname
+    fi
   else
-    cp -rp $i ~/$i
+    echo "No differences, continuing to next directory"
+  fi
+}
+
+for i in $(grep -v '^#' filelist); do
+  # If the entry is a file, we want to check the files underneath for differences
+  if [ -f $i ]; then
+    check_and_copy_file $i
+  elif [ -d $i ]; then
+    # Find to get the list of files and run check_and_copy_file on each file
+    for fname in $(find $i -type f); do
+      check_and_copy_file $fname
+    done
+  else
+    # This isn't a file or directory, so skip it
+    echo "Not a file or directory. Skipping"
   fi
 done
