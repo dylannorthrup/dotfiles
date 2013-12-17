fcf37f9376a1b66f22f322e60f2e117672a84f14
Dylan Northrup <dylan.northrup@turner.com>
2013-12-16 23:10:28 -0500

    Adding quotes to handle spaces in names

--- .tmux/foo.sh
+++ .tmux/foo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Create new window, split horizontally, then split left pane vertically
-tmux neww -n $(~/bin/random_line.rb ~/.tmux/window_names)
+tmux neww -n "$(~/bin/random_line.rb ~/.tmux/window_names)"
 tmux splitw -h -p 50 -t 0
 tmux splitw -v -p 50 -t 1
 tmux select-pane -t :.+
