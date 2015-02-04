5df45245a27207cd143d4cb1ec1157d94ea33eab
Dylan Northrup <dylan.northrup@turner.com>
2015-02-04 12:37:48 -0500

    Adding stuff to make pane ordering more sane

--- .tmux/bar.sh
+++ .tmux/bar.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-# Create new window, split horizontally, then split left pane vertically
+# Create new window, split vertically, then split both panes vertically, then 
+# kill pane 1 to make the C-n and C-p work as expected
 tmux neww -n "$(~/bin/random_line.rb ~/.tmux/window_names)"
 tmux splitw -v -p 50 -t 0
 tmux splitw -h -p 50 -t 0
+tmux splitw -h -p 50 -t 1
+tmux kill-pane -t 1
 tmux select-pane -t :.+
