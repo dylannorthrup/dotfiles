ab7c9941a1f9fb84ac660ea2808334d093fb6454
Dylan Northrup <dylan.northrup@turner.com>
2016-01-21 16:39:14 -0500

    Doing some tmux updating

--- .tmux/baz.sh
+++ .tmux/baz.sh
@@ -2,7 +2,7 @@
 # Create new window, split vertically, then split both panes vertically, then 
 # kill pane 1 to make the C-n and C-p work as expected
 tmux neww -n "$(~/bin/random_line.rb ~/.tmux/window_names)"
-tmux splitw -v -p 20 -t 0
+tmux splitw -v -p 50 -t 0
 tmux splitw -h -p 50 -t 0
 tmux splitw -h -p 50 -t 1
 tmux kill-pane -t 1
