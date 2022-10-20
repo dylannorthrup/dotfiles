2e6ffb624d823d2c271635e477121d6cf81ada56
Dylan Northrup <dylan@tockhq.com>
2022-10-20 17:12:44 -0500

    Adding mac conditional

--- .zsh/local.d/java.sh
+++ .zsh/local.d/java.sh
@@ -1,3 +1,12 @@
 # Java-specific settings
-export JAVA_HOME="/usr/lib/jvm/java-14-openjdk-amd64"
-export PATH=${PATH}:${JAVA_HOME}/bin
+if [[ ${UNAME_KERNEL} == "Linux" ]]; then
+  export JAVA_HOME="/usr/lib/jvm/java-14-openjdk-amd64"
+  export PATH=${PATH}:${JAVA_HOME}/bin
+elif [[ ${UNAME_KERNEL} == "Darwin" ]]; then
+  export JAVA_17_HOME="/Library/Java/JavaVirtualMachines/openjdk-17.jdk/Contents/Home"
+  export JAVA_HOME=$JAVA_17_HOME
+  export PATH=$JAVA_HOME/bin:$PATH
+else
+  echo "Not a configured Java platform"
+  return
+fi
