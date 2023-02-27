9d65603ae4bc1aa0cf101b576e0f444bf4fa537e
Dylan Northrup <github@doc-x.net>
2023-02-27 17:40:28 +0000

    Adding additional check now we are using terraform for inventory instead of our previous solution

--- .zsh/local.d/ansible.sh
+++ .zsh/local.d/ansible.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/zsh
 # shellcheck shell=bash
 # Ansible shortcuts
+function _preAnsibleTfTest() {
+  if [[ ! -d ./.terraform ]]; then
+    echo "### No .terraform directory. Running 'terraform init'."
+    terraform init
+    echo "### Terraform config regenerated. Running ansible command"
+  fi
+}
 alias amwin='ansible --list-hosts all | grep -v host | xargs mwin'
-alias ap='ansible-playbook --skip-tags require_master_branch'
-alias aptest='ansible-playbook -CD --skip-tags require_master_branch'
-alias lsah='ansible --list-hosts all | grep -v "hosts"'
-alias taglist='ap *.yml --list-tags'
+alias ap='_preAnsibleTfTest; ansible-playbook --skip-tags require_master_branch'
+alias aptest='_preAnsibleTfTest; ansible-playbook -CD --skip-tags require_master_branch'
+alias lsah='_preAnsibleTfTest; ansible --list-hosts all | grep -v "hosts"'
+alias taglist='_preAnsibleTfTest; ap *.yml --list-tags'
 plsah() {
   PROJ=$(basename "$(pwd)" | sed -e 's/^tock-//')
+  _preAnsibleTfTest
   ansible --list-hosts all | grep -v host | sed -e "s/$/.${PROJ}.tock/"
 }
 pamwin() {
   PROJ=$(basename "$(pwd)" | sed -e 's/^tock-//')
+  _preAnsibleTfTest
   ansible --list-hosts all | grep -v host | sed -e "s/$/.${PROJ}.tock/" | xargs mwin
 }
