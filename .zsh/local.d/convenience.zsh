6731e9c40d3108a9d869980463696e2eba785257
Dylan Northrup <github@doc-x.net>
2023-04-19 13:18:52 +0000

    printf on the input made things brittle; shellcheck suggestions

--- .zsh/local.d/convenience.zsh
+++ .zsh/local.d/convenience.zsh
@@ -23,15 +23,15 @@ function commafy_num() {
 }
 
 function displaytime() {
-  local T=$(printf -v int %.0f "$1")
+  local T=${1}
   local D=$((T/60/60/24))
   local H=$((T/60/60%24))
   local M=$((T/60%60))
   local S=$((T%60))
-  (( $D > 0 )) && printf '%d days ' $D
-  (( $H > 0 )) && printf '%d hours ' $H
-  (( $M > 0 )) && printf '%d minutes ' $M
-  (( $D > 0 || $H > 0 || $M > 0 )) && printf 'and '
+  (( D > 0 )) && printf '%d days ' $D
+  (( H > 0 )) && printf '%d hours ' $H
+  (( M > 0 )) && printf '%d minutes ' $M
+  (( D > 0 || H > 0 || M > 0 )) && printf 'and '
   printf '%d seconds\n' $S
 }
 
