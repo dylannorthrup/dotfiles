5108ac9bb50411f765bd147b733c36c0473a8b99
Dylan Northrup <northrup@ufl.edu>
2024-01-18 09:25:07 -0500

    Removing completion for deprecated command

--- .zsh/completions.d/local.sh
+++ .zsh/completions.d/local.sh
@@ -13,14 +13,3 @@ compctl -W ${TOCKTIX_PATH}/ -/ cdtt
 
 bindkey "^[OA" up-history
 bindkey "^[OB" down-history
-
-
-_adr() {
-  # Autocomplete only when entering the last term
-  if [ ${COMP_CWORD} -eq $((${#COMP_WORDS[@]} - 1)) ]
-  then
-    COMPREPLY=( $(_adr_autocomplete ${COMP_WORDS[*]} ) )
-  fi
-}
-
-complete -F _adr -o default adr
